1e1de28a1c75241683918dca0b831c51ad457a54
Specmatic Renovate <github-service-account@specmatic.io>
2026-01-17 16:07:43 +0000

    chore(deps): update dependency ch.qos.logback:logback-classic to v1.5.25

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
 
     // Logging
-    implementation("ch.qos.logback:logback-classic:1.5.24")
+    implementation("ch.qos.logback:logback-classic:1.5.25")
     implementation("io.github.microutils:kotlin-logging-jvm:3.0.5")
 
     // Coroutines
