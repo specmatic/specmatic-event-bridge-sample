305547cc5322664af7fcd50313bb5b48bbbcbfe3
specmatic-builder <github-service-account@specmatic.io>
2026-02-05 10:08:14 +0530

    chore(deps): update dependency aws.sdk.kotlin:eventbridge to v1.6.10 (#72)

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
     testImplementation("org.assertj:assertj-core:3.27.7")
 
     // AWS SDK for EventBridge
-    implementation("aws.sdk.kotlin:eventbridge:1.6.9")
+    implementation("aws.sdk.kotlin:eventbridge:1.6.10")
     implementation("aws.sdk.kotlin:sqs:1.6.9")
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.6.1")
 
