21dea15848dd6112c236ac110a8ebfda17fa139b
specmatic-builder <github-service-account@specmatic.io>
2026-02-19 13:49:50 +0530

    chore(deps): update dependency aws.sdk.kotlin:sqs to v1.6.20 (#103)

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 
     // AWS SDK for EventBridge
     implementation("aws.sdk.kotlin:eventbridge:1.6.20")
-    implementation("aws.sdk.kotlin:sqs:1.6.19")
+    implementation("aws.sdk.kotlin:sqs:1.6.20")
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.6.4")
 
     // Kafka client
