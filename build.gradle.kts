278944f05fccb7248d274f439f5b4f03b0639c07
Specmatic Renovate <github-service-account@specmatic.io>
2026-04-16 00:36:28 +0000

    chore(deps): update dependency org.apache.kafka:kafka-clients to v4

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,7 +33,7 @@ dependencies {
     implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.6.10")
 
     // Kafka client
-    implementation("org.apache.kafka:kafka-clients:3.9.2")
+    implementation("org.apache.kafka:kafka-clients:4.2.0")
 
     // JSON serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
