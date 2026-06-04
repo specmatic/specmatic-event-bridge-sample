07e6cd7b6666dda9e949d7a389f7864653f67355
Specmatic Renovate <github-service-account@specmatic.io>
2026-06-04 00:57:24 +0000

    chore(deps): update kotlin to v2.4.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    kotlin("jvm") version "2.3.21"
-    kotlin("plugin.serialization") version "2.3.21"
+    kotlin("jvm") version "2.4.0"
+    kotlin("plugin.serialization") version "2.4.0"
     application
 }
 group = "io.specmatic.async"
