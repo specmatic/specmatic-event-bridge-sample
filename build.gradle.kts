d8a22d4d190fa6c4effc8e6e883626fd6975fd98
specmatic-builder <github-service-account@specmatic.io>
2026-09-03 05:59:14 +0530

    chore(deps): update dependency aws.smithy.kotlin:http-client-engine-okhttp to v1.7.9 (#215)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     // AWS SDK for EventBridge
     implementation("aws.sdk.kotlin:eventbridge:1.8.41")
     implementation("aws.sdk.kotlin:sqs:1.8.41")
-    implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.7.8")
+    implementation("aws.smithy.kotlin:http-client-engine-okhttp:1.7.9")
 
     // Kafka client
     implementation("org.apache.kafka:kafka-clients:3.9.2")
