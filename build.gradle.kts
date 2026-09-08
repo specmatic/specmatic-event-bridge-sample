3493a842d2c80c3e758c765459316a2e24c1fd47
Specmatic Renovate <github-service-account@specmatic.io>
2026-09-08 00:28:40 +0000

    chore(deps): update kotlin to v2.4.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    kotlin("jvm") version "2.4.10"
+    kotlin("jvm") version "2.4.20"
     kotlin("plugin.serialization") version "2.4.10"
     application
 }
